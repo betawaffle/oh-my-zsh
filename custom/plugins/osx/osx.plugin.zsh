767e2f316280ee29688b9555ba39fe6a371f99ef
Andrew Hodges <betawaffle@gmail.com>
2011-05-29 10:02:33 -0400

    Override OS X Plugin
    Copy my customizations from the OS X plugin. Add other alias(es). All of this
    to override the unwanted stuff in the standard plugin.

---
+++ custom/plugins/osx/osx.plugin.zsh
@@ -0,0 +1,8 @@
+alias showfiles='defaults write com.apple.finder AppleShowAllFiles TRUE; killall Finder'
+alias hidefiles='defaults write com.apple.finder AppleShowAllFiles FALSE; killall Finder'
+
+# Recursively delete .DS_Store files
+alias rm-dsstore="find . -name '*.DS_Store' -type f -delete"
+
+# Open!
+alias o='open'
