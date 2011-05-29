23bf33c1843c96cc9ad486f0c5b3b2547be1c35a
Andrew Hodges <betawaffle@gmail.com>
2011-05-29 10:12:02 -0400

    Disable Grep Color
    Grep color can cause errors in some scripts, disable it.

--- lib/grep.zsh
+++ lib/grep.zsh
@@ -2,5 +2,6 @@
 # Color grep results
 # Examples: http://rubyurl.com/ZXv
 #
-export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='1;32'
+# TODO: This should be configurable, as it can cause errors.
+# export GREP_OPTIONS='--color=auto'
+# export GREP_COLOR='1;32'
