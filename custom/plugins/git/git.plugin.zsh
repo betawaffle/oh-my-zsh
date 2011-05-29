8214bb90da555054cc84d32a6fdb4ca78111c912
Andrew Hodges <betawaffle@gmail.com>
2011-05-29 10:05:04 -0400

    Git Plugin
    Copy some aliases from standard plugin. Add some custom aliases. Copy
    current_branch for compatability.

---
+++ custom/plugins/git/git.plugin.zsh
@@ -0,0 +1,40 @@
+alias g='git'
+
+compdef g=git
+
+alias ga='git add'
+alias gb='git branch'
+alias gc='git commit -v'
+alias gd='git difftool'
+alias gf='git fetch'
+alias gl='git lg'
+alias gm='git merge --no-ff'
+alias gp='git difftool --cached'
+alias gs='git status -s'
+alias gx='gitx'
+
+compdef _git ga=git-add
+compdef _git gb=git-branch
+compdef _git gc=git-commit
+compdef _git gd=git-difftool
+compdef _git gf=git-fetch
+compdef _git gl=git-log
+compdef _git gm=git-merge
+compdef _git gp=git-difftool
+compdef _git gs=git-status
+
+alias gps='git diff --cached --stat'
+
+compdef _git gps=git-diff
+
+alias branches='git branch -a'
+alias stashes='git stash list'
+
+function stashes-deleted {
+	git fsck --unreachable | grep commit | cut -d\  -f3 | xargs git lg --merges --no-walk --grep=WIP
+}
+
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
