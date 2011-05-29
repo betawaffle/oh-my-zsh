8db17d982efd33ad497f14e3972724a4271e38a8
Andrew Hodges <betawaffle@gmail.com>
2011-05-29 10:13:22 -0400

    Corrections
    Remove auto-correct prompts on some tools I use frequently.

---
+++ custom/correction.zsh
@@ -0,0 +1,8 @@
+# Haskell
+alias cabal='nocorrect cabal' # Keep it from trying to "correct" to .cabal while in ~
+
+# Node
+alias npm='nocorrect npm'
+
+# Kaleidoscope
+alias ksdiff='nocorrect ksdiff'
